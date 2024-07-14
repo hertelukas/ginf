8f53c0a1c50cf7e30302cd7c7068a36d3628ed37
Lukas <lukas.hertel@tum.de>
2024-07-14 21:37:13 +0200

    backend: Added fileTags relation to models

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,6 +1,6 @@
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
+#[derive(Identifiable, Queryable, Selectable)]
 #[diesel(table_name = crate::schema::files)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct File {
@@ -9,7 +9,7 @@ pub struct File {
     pub name: String,
 }
 
-#[derive(Queryable, Selectable)]
+#[derive(Identifiable, Queryable, Selectable)]
 #[diesel(table_name = crate::schema::tags)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Tag {
@@ -17,3 +17,14 @@ pub struct Tag {
     pub tag: String,
 }
 
+
+#[derive(Identifiable, Queryable, Selectable, Associations)]
+#[diesel(belongs_to(Tag))]
+#[diesel(belongs_to(File))]
+#[diesel(primary_key(file_id, tag_id))]
+#[diesel(table_name = crate::schema::file_tags)]
+pub struct FileTags {
+    pub rowid: i32,
+    pub file_id: i32,
+    pub tag_id: i32
+}
